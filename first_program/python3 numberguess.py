a7b64e0df0453ddfe247da7c9d7e83fdc2cd3607
huangpuyuan <huangpuyuan@outlook.com>
2018-08-26 23:54:13 +0800

    guess number

---
+++ first_program/python3 numberguess.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Author kaitai--HPY
+
+This is a temporary script file.
+"""
+
+import random
+
+def main():
+    '''Input the bounds of the range of numberss and lets the user guess the compouter's 
+    number until the guess i s correct. '''
+    smaller = int(input("Enter the smaller number: "))
+    larger =  int(input("Enter the larger number: "))
+    myNumber = random.randint(smaller,larger)
+    count = 0
+    while True:
+        count +=1
+        userNumber = int(input("Enter your guess: "))
+        if userNumber < myNumber:
+            print("Too small")
+        elif userNumber > myNumber:
+            print("Too larger")
+        else:
+            print("You've got it in", count,"tries!")
+            break
+        
+if __name__ == "__main__":
+    main()
+    
+    
